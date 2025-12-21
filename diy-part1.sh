825ff0fdbf6a97b7e414572de1195e2aa03652d0
god777 <64050648+ffff3333@users.noreply.github.com>
2025-12-21 12:01:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,7 +28,7 @@
 #sed -i '1i src-git passwall_packages https://github.com/ffff3333/openwrt-passwall-packages' feeds.conf.default
 #sed -i '1i src-git passwall-packages https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
 #sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-#sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '1i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
 #sed -i '4i src-git argon https://github.com/ffff3333/themes' feeds.conf.default
-sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default

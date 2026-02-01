b8e071e2bc9428a5ad323ea947faf127310428a2
god777 <64050648+ffff3333@users.noreply.github.com>
2026-02-01 09:25:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -37,5 +37,5 @@ sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.
 #sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '4i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.default
 #sed -i '5i src-git immortalwrt_packages https://github.com/immortalwrt/packages' feeds.conf.default
-sed -i '5i src-git lang https://github.com/flrz/lang' feeds.conf.default
-sed -i '6i src-git openwrt-passwall-packages https://github.com/ffff3333/openwrt-passwall-packages' feeds.conf.default
+#sed -i '5i src-git lang https://github.com/flrz/lang' feeds.conf.default
+#sed -i '6i src-git openwrt-passwall-packages https://github.com/ffff3333/openwrt-passwall-packages' feeds.conf.default

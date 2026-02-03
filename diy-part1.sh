2dcd2f2b75f22a10f6a8e7514c4c847e99af2375
god777 <64050648+ffff3333@users.noreply.github.com>
2026-02-03 13:19:35 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,14 +26,14 @@
 
 # Add a feed source
 #sed -i '1i src-git immortalwrt_packages https://github.com/immortalwrt/packages' feeds.conf.default
-sed -i '1i src-git passwall_packages https://sourceforge.net/projects/openwrt-passwall-build/files/releases/packages-24.10/x86_64/passwall_packages/' feeds.conf.default
-sed -i '2i src-git passwall_luci https://sourceforge.net/projects/openwrt-passwall-build/files/releases/packages-24.10/x86_64/passwall_luci/' feeds.conf.default
+#sed -i '1i src-git passwall_packages https://sourceforge.net/projects/openwrt-passwall-build/files/releases/packages-24.10/x86_64/passwall_packages/' feeds.conf.default
+#sed -i '2i src-git passwall_luci https://sourceforge.net/projects/openwrt-passwall-build/files/releases/packages-24.10/x86_64/passwall_luci/' feeds.conf.default
 #sed -i '1i src-git flrz https://github.com/flrz/passwall_packages' feeds.conf.default
 #sed -i '2i src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
-#sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
-#sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '4i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.default
+#ed -i '3i src-git openclash https://github.com/vernesong/OpenClash' feeds.conf.default
+sed -i '1i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '2i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '4i src-git flrz https://github.com/flrz/openwrt-packages' feeds.conf.default
 #sed -i '5i src-git golang https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang' feeds.conf.default
 #sed -i '5i src-git lang https://github.com/flrz/lang' feeds.conf.default
 #sed -i '6i src-git openwrt-passwall-packages https://github.com/ffff3333/openwrt-passwall-packages' feeds.conf.default

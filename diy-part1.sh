d4edbd0efc811f1a758c111f4c03e7905aa9d73b
god777 <64050648+ffff3333@users.noreply.github.com>
2026-04-19 20:06:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,4 +3,5 @@ cat >> feeds.conf.default << EOF
 src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main
 src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main
 src-git openclash https://github.com/vernesong/OpenClash.git;master
+src-git argon https://github.com/jerrykuku/luci-theme-argon.git;master
 EOF
